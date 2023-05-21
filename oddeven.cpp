6a08fae2826958d254cfc5004a9e27a3646f0065
AMAN SINGH <134042492+Santaji001@users.noreply.github.com>
2023-05-21 16:30:19 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+ using namespace std;
+ 
+    int main()        
+ {  
+//your creativity->
+   
+   int x;
+   cin>>x;
+
+    if (x%2==0)
+    cout<<"even";
+     else
+    cout<<"odd";
+    
+     return 0;
+
+}
---
+++ saving.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+ using namespace std;
+ 
+    int main()        
+ {  
+//your creativity->
+      int saving;
+      cin>>saving;
+
+        if (saving>10000) 
+        {
+          
+           if (saving>15000)
+           cout<<"sneha";
+
+           else if (saving>12000)
+           cout<<"rashmi";
+           else 
+           cout <<"alone";
+       }
+       else 
+       cout<<"friends";
+     
+    
+     return 0;
+}
